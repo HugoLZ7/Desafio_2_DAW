390e8c362a900ffea01a8e78db2e57ad705f4d5c
HugoLZ7 <destroyermaster10@gmail.com>
2025-04-05 15:02:44 -0600

    Add function to create customer table

--- js/main.js
+++ js/main.js
@@ -20,6 +20,8 @@
         };
         // Agregar el cliente al array
         clientes.push(nuevoCliente);
+        actualizarTabla();
+
 
         //Pruebas de obtencion de datos en consola
         /*
@@ -28,7 +30,33 @@
         console.log(telefono);
         console.log(correo);
         */
-       
+        function actualizarTabla() {
+            const clientesList = document.getElementById('clientesList');
+            clientesList.innerHTML = ''; // Limpiar la tabla
+        
+            clientes.forEach((cliente, index) => {
+                const totalAPagar = cliente.interesMensual + cliente.montoPrestado; // Calcular total a pagar
+                const newRow = document.createElement('tr');
+                newRow.innerHTML = `
+                    <td>${cliente.nombre}</td>
+                    <td>${cliente.dni}</td>
+                    <td>${cliente.telefono}</td>
+                    <td>${cliente.correo}</td>
+                    <td>${cliente.interesMensual.toFixed(2)} $</td>
+                    <td>${totalAPagar.toFixed(2)} $</td> <!-- Nueva columna para total a pagar -->
+                    <td>
+                        <button class="btn btn-info" data-toggle="modal" data-target="#prestamoModal" onclick="abrirModal(${index})" ${cliente.tienePrestamo ? 'disabled' : ''}>
+                            ${cliente.tienePrestamo ? 'Préstamo Activo' : 'Registrar Préstamo'}
+                        </button>
+                        <button class="btn btn-warning" onclick="marcarComoPagado(${index})" ${!cliente.tienePrestamo ? 'disabled' : ''}>
+                            Marcar como Pagado
+                        </button>
+                    </td>
+                `;
+                clientesList.appendChild(newRow);
+            });
+        }
+        
         // Limpiar el formulario
         document.getElementById("clienteForm").reset();
     });
